b075bb82b62e54d406781e3a0e940fda6a931368
DTiveron <dtiveron@dist113.org>
2026-08-18 14:03:05 -0500

    Add files via upload

---
+++ Fish.java
@@ -0,0 +1,26 @@
+package aquarium.creatures;
+
+public class Fish extends SeaCreature {
+
+    private String symbol;
+
+    public Fish(String name, int position, int speed, int direction, String symbol) {
+        super(name, position, speed, direction);
+        this.symbol = symbol;
+    }
+
+    @Override
+    public void move(int tankWidth) {
+        position += speed * direction;
+        keepInsideTank(tankWidth);
+    }
+
+    @Override
+    public String getSymbol() {
+        return direction >= 0 ? symbol : reverseSymbol(symbol);
+    }
+
+    private String reverseSymbol(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
+}
